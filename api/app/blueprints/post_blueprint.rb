6160e98e5d3a65bd9ddfbbb42f66eb1d4333aeb9
Matt Magin <accounts@magin.tech>
2026-02-06 23:32:52 +1000

    Delete blueprinter from api - not needed

--- api/app/blueprints/post_blueprint.rb
+++
@@ -1,8 +0,0 @@
-class PostBlueprint < Blueprinter::Base
-    identifier :id
-    fields :slug, :title, :content, :published_at
-
-    field :reading_time do |post|
-        ReadingTime.calculate(post.content)
-    end
-end
