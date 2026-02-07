028cb409dda379f06bb64ddbd8648b86f719f892
Matt Magin <accounts@magin.tech>
2026-02-07 19:04:18 +1000

    Pull resume from rails api

--- web/src/pages/Resume.tsx
+++ web/src/pages/Resume.tsx
@@ -2,10 +2,21 @@ import { Download } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import ResumeRenderer from '@/components/ResumeRenderer'
 import { useStrings } from '@/content'
-import resumeContent from '@/content/resume.md?raw'
+import { useResume } from '@/hooks/queries'
 
 const Resume = () => {
   const { resume: resumeStrings } = useStrings()
+  const { data: resumeContent, isLoading: resumeLoading } = useResume()
+
+  // TODO: make these look nicer
+  // TODO: we have an inital flash of loading before the resume is loaded, need fix and make nicer..
+  if (resumeLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (!resumeContent) {
+    return <div>No resume content found</div>
+  }
 
   return (
     <div className="mx-auto max-w-4xl px-6 py-16">
