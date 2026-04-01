1fbdd1417386675bdb578468a2627510362a4daa
Matt Magin <accounts@magin.tech>
2026-04-01 16:52:15 +1000

    Fix merge issue

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,17 +5,11 @@ import NotFound from './NotFound'
 const Pages: React.FC = () => {
   return (
     <Routes>
-<<<<<<< HEAD
       {Object.values(ROUTE_DEFINITIONS)
         .filter((r): r is typeof r & { component: React.ComponentType } => !!r.component)
         .map(({ path, component: Component }) => (
           <Route key={path} path={path} element={<Component />} />
         ))}
-=======
-      {Object.values(ROUTE_DEFINITIONS).map(({ path, component: Component }) => (
-        <Route key={path} path={path} element={<Component />} />
-      ))}
->>>>>>> 2181ca3c (Small fixes)
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
